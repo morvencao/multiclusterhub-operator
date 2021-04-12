fbc41f853075d0001880944bc2a931d7f6615944
Nathan Weatherly <nweather@redhat.com>
2021-04-12 13:47:06 -0400

    forgot this renaming

--- mock-component-image/templates/foundation-proxyserver.go
+++ mock-component-image/templates/foundation-proxyserver.go
@@ -6,8 +6,12 @@ import (
 	"net/http"
 )
 
+func ProxyServerMockPing(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
+
 func main() {
-	http.HandleFunc("/healthz", Ping)
+	http.HandleFunc("/healthz", ProxyServerMockPing)
 
 	server := &http.Server{
 		Addr:      ":6443",
